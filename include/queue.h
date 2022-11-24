6e2b727594f51a3da6234c441d7c25009dfca58b
mixa <mihsa11ru@gmail.com>
2022-11-24 22:32:58 +0300

    queue_fix

--- include/queue.h
+++ include/queue.h
@@ -1,14 +1,13 @@
 #include "stdvector.hpp"
 template <typename T> class queue: public stdvector<T>{
 private:
-    int head=0,tail=0;
+    T* head, *tail;
 public:
     queue(int n=100000): stdvector<T>(n){
         this->size=0;
     }
     void resize(int n_size)=delete;
     
-    void set_cap(int n_cap)=delete;
     
     T& operator [](int i)=delete;
     
@@ -16,18 +15,24 @@ public:
     
     bool empty() {return (this->size==0);}
     
-    
     T front(){
         if (this->empty()) throw std::out_of_range("queue is empty");
-        return this->data[this->head];
+        return *head;
+        
     }
     void push(T a){
-        this->push_back(a);
-        this->tail=(this->tail+1)%this->capacity;
+        if (tail==head){
+            this->set_cap(this->capacity*2);
+            head = this->data;
+            tail = this->data + this->size;
+        }
+        *tail = a;
+        tail = this->data + (tail - this->data + 1) % this->capacity;
+        this->size++;
     }
     void pop(){
         if (this->empty()) throw std::out_of_range("queue is empty");
-        this->head=(this->head+1)%this->capacity;
+        head = this->data+(head - this->data + 1)%this->capacity;
         this->size--;
     }
 };
